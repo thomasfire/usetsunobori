b023d546b9247ce9969f03556b6a5d7c02c4e39a
Thomas Fire <tomasfire@live.com>
2021-06-17 03:09:11 +0300

    Start work on EMG server

---
+++ Content/omg_serv/omg_serv/omg_serv.cpp
@@ -0,0 +1,16 @@
+﻿// omg_serv.cpp : Defines the entry point for the application.
+//
+
+#include "omg_serv.h"
+
+#include "SerialPort.h"
+
+#include <stdio.h>
+#include <thread>
+
+struct omg_serial_serv_t{
+	SerialPort* file_handler;
+	omg_serial_input_callback_f input_callback;
+	std::thread* thr_handler;
+	unsigned state;
+};
